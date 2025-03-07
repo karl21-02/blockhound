182fa3bc1c25b67281439100c9cc2cd0783d21c5
kimjunhee <manuna530@gmail.com>
2025-03-07 11:28:18 +0900

    INIT

--- src/main/java/com/kangwon/blockhound/BlockhoundApplication.java
+++ src/main/java/com/kangwon/blockhound/BlockhoundApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlockhoundApplication {
 
     public static void main(String[] args) {
+        Blockhoun.install();
         SpringApplication.run(BlockhoundApplication.class, args);
     }
 
