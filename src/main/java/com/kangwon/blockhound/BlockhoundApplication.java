b5c4fbacff06e52383d0677efdd0d91f94c3222b
kimjunhee <manuna530@gmail.com>
2025-03-07 11:26:58 +0900

    Initial commit

---
+++ src/main/java/com/kangwon/blockhound/BlockhoundApplication.java
@@ -0,0 +1,13 @@
+package com.kangwon.blockhound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockhoundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockhoundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kangwon/blockhound/BlockhoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kangwon.blockhound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockhoundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
